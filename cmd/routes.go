3d77e35f248feba59d59981262c54f27f6a00211
iiitvamsi <bendalamkrishnavamsi@gmail.com>
2023-02-16 18:01:59 +0530

    products issue resolution

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/kvbendalam/webservices/handlers"
+	"github.com/kvbendalam/golangproducts/database"
+	"github.com/kvbendalam/golangproducts/models"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("/", handlers.ListProducts)
-	// app.Post("/fact", handlers.CreateFact)
-	// app.Get("/fact/:id", handlers.GetFactById)
-	// app.Delete("/fact/:id", handlers.DeleteFact)
+	app.Get("/product", ListProducts)
+	app.Post("/product", CreateProduct)
+	app.Get("/product/:id", GetProductById)
+	app.Delete("/product/:id", DeleteProduct)
+}
+
+func ListProducts(c *fiber.Ctx) error {
+	products := []models.Product{}
+	database.DB.Db.Find(&products)
+	return c.Status(200).JSON(products)
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	product := new(models.Product)
+	if err := c.BodyParser(product); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	database.DB.Db.Create(&product)
+	return c.Status(200).JSON(product)
+}
+
+func GetProductById(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var products models.Product
+
+	result := database.DB.Db.Find(&products, id)
+
+	if result.RowsAffected == 0 {
+		return c.SendStatus(404)
+	}
+
+	return c.Status(200).JSON(&products)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var product models.Product
+
+	if result := database.DB.Db.First(&product, id); result.Error != nil {
+		fmt.Println(result.Error)
+	}
+
+	database.DB.Db.Delete(&product)
+
+	return c.Status(200).JSON(&product)
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,11 @@
-module github.com/kvbendalam/golang-products
+module github.com/kvbendalam/golangproducts
 
 go 1.16
 
 require (
 	github.com/gofiber/fiber v1.14.6
 	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/kvbendalam/golang-products v0.0.0-20230216120304-e57513edf549
 	github.com/kvbendalam/webservices v0.0.0-20230209101817-8484fa1d9cd2
 	gorm.io/driver/postgres v1.4.7
 	gorm.io/gorm v1.24.5
